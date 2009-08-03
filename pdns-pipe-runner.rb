0fcee7d38f0733b7dc32f7fac15b9cac5fe6c5eb
rip <rip@7390e61a-850e-11de-8eb9-d39840a433f0>
2009-08-03 17:26:59 +0000

    add a --config option

--- pdns-pipe-runner.rb
+++ pdns-pipe-runner.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/ruby
 
 require 'pdns.rb'
+require 'getoptlong'
 
-Pdns::Runner.new("/etc/pdns/pdns-ruby-backend.cfg")
+opts = GetoptLong.new(
+    [ '--config', '-c', GetoptLong::REQUIRED_ARGUMENT]
+)
+
+conffile = "/etc/nagger/nagger.cfg"
+
+opts.each do |opt, arg|
+    case opt
+        when '--config'
+            conffile = arg
+    end
+end
+
+
+Pdns::Runner.new(conffile)

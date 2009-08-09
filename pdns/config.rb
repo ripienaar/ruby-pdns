f9b0f2e2cf26fd470ed6c3f42761b2d64d454273
rip@devco.net <rip@devco.net@7390e61a-850e-11de-8eb9-d39840a433f0>
2009-08-09 21:28:22 +0000

    First stab at pulling out the config related code into Pdns::Config, the runner will initialize it and a copy will be saved in Pdns.config where other classes can get it out when needed
    Update issue 3

---
+++ pdns/config.rb
@@ -0,0 +1,78 @@
+module Pdns
+    # Simple class to handle the configuring of the framework.
+    #
+    # At present config files can look like this, showing defaults:
+    #
+    #    logfile = /var/log/pdns/pipe-backend.log
+    #    loglevel = info|error|warn|fatal|debug
+    #    records_dir = /etc/pdns/records
+    #    soa_contact = unconfigured.ruby.pdns.server
+    #    soa_nameserver = unconfigured.ruby.pdns.server
+    #    reload_interval = 60
+    #    keep_logs = 10
+    #    max_log_size = 1024000
+    #
+    class Config
+        attr_reader :logfile, :loglevel, :records_dir, :soa_contact, :soa_nameserver, :reload_interval, :keep_logs, :max_log_size
+
+        def initialize(configfile)
+            @logfile => "/var/log/pdns/pipe-backend.log"
+            @loglevel => Logger::INFO
+            @records_dir => "/etc/pdns/pipe_records"
+            @soa_contact => "unconfigured.ruby.pdns.server"
+            @soa_nameserver => "unconfigured.ruby.pdns.server"
+            @reload_interval => 60
+            @keep_logs => 10
+            @max_log_size => 1024000
+
+
+            if File.exists?(configfile)
+                File.open(configfile, "r").each do |line|
+                    unless line =~ /^#|^$/
+                        if (line =~ /(.+?)\s*=\s*(.+)/)
+                            key = $1
+                            val = $2
+
+                            case key
+                                when "logfile"
+                                    @logfile = val
+                                when "records_dir"
+                                    @records_dir = val
+                                when "soa_contact"
+                                    @soa_contact = val
+                                when "soa_nameserver"
+                                    @soa_nameserver = val
+                                when "reload_interval"
+                                    @reload_interval = val.to_i
+                                when "keep_logs"
+                                    @keep_logs = val.to_i
+                                when "max_log_size"
+                                    @max_log_size = val.to_i
+                                when "loglevel"
+                                    case val
+                                        when "info"
+                                            @loglevel = Logger::INFO
+                                        when "warn"
+                                            @loglevel = Logger::WARN
+                                        when "debug"
+                                            @loglevel = Logger::DEBUG
+                                        when "fatal"
+                                            @loglevel = Logger::FATAL
+                                        when "error"
+                                            @loglevel = Logger::ERROR
+                                        end
+                                else
+                                    raise("Unknown config parameter #{key}")
+                            end
+                        end
+                    end
+                end
+            end
+
+            Pdns.config = self
+            self
+        end
+    end
+end
+
+# vi:tabstop=4:expandtab:ai

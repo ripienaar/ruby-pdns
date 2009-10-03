5dc511c6e82f8721796f1eb5b125f35c26e07d7b
rip@devco.net <rip@devco.net@7390e61a-850e-11de-8eb9-d39840a433f0>
2009-10-03 18:17:07 +0000

    Update issue 2 Add the option to report stats in micro seconds rather than seconds, rrd does not like fractions

--- ruby-pdns/sbin/pdns-get-stats.rb
+++ ruby-pdns/sbin/pdns-get-stats.rb
@@ -5,16 +5,20 @@ require 'pdns'
 
 opts = GetoptLong.new(
     [ '--record', '-r', GetoptLong::REQUIRED_ARGUMENT],
+    [ '--usec', GetoptLong::NO_ARGUMENT],
     [ '--config', '-c', GetoptLong::REQUIRED_ARGUMENT]
 )
 
 conffile = "/etc/pdns/pdns-ruby-backend.cfg"
 record = nil
+multiplier = 1
 
 opts.each do |opt, arg|
     case opt
         when '--record'
             record = arg
+        when '--usec'
+            multiplier = 1000000
         when '--config'
             conffile = arg
         end
@@ -34,7 +38,10 @@ begin
     if stats.include_record?(record)
         r = stats.recordstats(record)
 
-        puts("usagecount:#{r[:usagecount]} totaltime:#{r[:totaltime]}")
+        totaltime = r[:totaltime] * multiplier
+        totaltime = totaltime.to_i if multiplier == 1000000
+
+        puts("usagecount:#{r[:usagecount]} totaltime:#{totaltime}")
     else
         raise("Cannot find stats for record #{record}")
     end
